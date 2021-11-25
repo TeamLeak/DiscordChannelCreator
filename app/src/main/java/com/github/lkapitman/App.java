15156033c79f1fa45f44825dd08617fd79df3c69
CarryLove <l.kapitman@gmail.com>
2021-11-25 23:34:32 +0300

    Перешёл на Discord4J, создана архитектура. Сделал базовую команду

--- app/src/main/java/com/github/lkapitman/App.java
+++ app/src/main/java/com/github/lkapitman/App.java
@@ -1,12 +1,42 @@
 package com.github.lkapitman;
 
+import com.github.lkapitman.command.Command;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
-    public String getGreeting() {
-        return "Hello World!";
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    static {
+        commands.put("ping", event -> event.getMessage()
+                .getChannel().block()
+                .createMessage("Pong!").block());
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(final String[] args) {
+        final GatewayDiscordClient client = DiscordClientBuilder.create("ODg4MDM4NjE2OTEwMTY0MDA5.YUM4aQ.bHQdtK8bD4O-pleDVWT5KtpbPU4").build()
+                .login()
+                .block();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+
+                .subscribe(event -> {
+                    final String content = event.getMessage().getContent();
+
+                    for (final Map.Entry<String, Command> entry : commands.entrySet()) {
+                        if (content.startsWith('!' + entry.getKey())) {
+                            entry.getValue().execute(event);
+                            break;
+                        }
+                    }
+                });
+
+        client.onDisconnect().block();
     }
+
 }
---
+++ app/src/main/java/com/github/lkapitman/command/Command.java
@@ -0,0 +1,9 @@
+package com.github.lkapitman.command;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+
+public interface Command {
+
+    void execute(MessageCreateEvent event);
+
+}
