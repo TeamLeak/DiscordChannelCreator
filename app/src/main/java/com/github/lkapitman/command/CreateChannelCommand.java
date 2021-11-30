f140cd28a1ba390ceeb15c19d512d9e28ec71e46
CarryLove <l.kapitman@gmail.com>
2021-11-30 16:15:50 +0300

    Исправлен баг. вынесен if/else

--- app/src/main/java/com/github/lkapitman/command/CreateChannelCommand.java
+++ app/src/main/java/com/github/lkapitman/command/CreateChannelCommand.java
@@ -5,8 +5,6 @@ import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.channel.ServerTextChannelBuilder;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.channel.ServerVoiceChannelBuilder;
-import org.javacord.api.entity.server.invite.Invite;
-import org.javacord.api.entity.server.invite.InviteBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
@@ -16,40 +14,33 @@ public class CreateChannelCommand implements MessageCreateListener {
     public void onMessageCreate(MessageCreateEvent event) {
         final String[] args = event.getMessage().getContent().split("\\s");
 
-        if (!(args.length > 3)) {
-            event.getChannel().sendMessage("Правильное использование: \n ;create <name> <people-count> <type> ");
-            return;
-        }
-        String type = args[3];
-        String maxCount = args[2];
-
         if (event.getMessage().getContent().startsWith(";create")) {
-            Invite invite;
-            
-            switch (type) {
-                case "voice" -> {
-                    ServerVoiceChannel voice = new ServerVoiceChannelBuilder(event.getServer().get()).setName(args[1]).create().join();
+            if (!(args.length > 2)) {
+                event.getChannel().sendMessage("Правильное использование: \n ;create <name> <text/voice> ");
+                return;
+            }
+        }
 
-                    event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
-                    invite = new InviteBuilder(voice).setMaxAgeInSeconds(60*60*24)
-                            .setMaxUses(Integer.parseInt(maxCount))
-                            .create().join();
+        String type = args[2];
 
-                    event.getChannel().sendMessage(invite.getUrl().toString());
-                }
-                case "text" -> {
-                    ServerTextChannel text = new ServerTextChannelBuilder(event.getServer().get()).setName(args[1]).create().join();
+        switch (type) {
+            case "voice" -> {
+                ServerVoiceChannel voice = new ServerVoiceChannelBuilder(event.getServer().get()).setName(args[1]).create().join();
 
-                    event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
-                    invite = new InviteBuilder(text).setMaxAgeInSeconds(60*60*24)
-                            .setMaxUses(Integer.parseInt(maxCount))
-                            .create().join();
+                event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
+            }
+            case "text" -> {
+                ServerTextChannel text = new ServerTextChannelBuilder(event.getServer().get()).setName(args[1]).create().join();
 
-                    event.getChannel().sendMessage(invite.getUrl().toString());
-                }
+                event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
+            }
+            default -> {
+                event.getChannel().sendMessage(
+                        "Неправильный тип! \n" +
+                        "text - текстовый \n" +
+                        "voice - голосовой");
+                event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsdown:"));
             }
         }
     }
-
-
 }
