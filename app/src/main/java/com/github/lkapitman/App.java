b1c510f9d921b7b38fc7ad44f954ea61f867df2a
CarryLove <l.kapitman@gmail.com>
2021-11-30 21:43:38 +0300

    Исправил логги. Теперь можно создавать во всех каналах

--- app/src/main/java/com/github/lkapitman/App.java
+++ app/src/main/java/com/github/lkapitman/App.java
@@ -2,24 +2,40 @@ package com.github.lkapitman;
 
 import com.github.lkapitman.command.CreateChannelCommand;
 import com.github.lkapitman.command.PingCommand;
+import com.github.lkapitman.util.javacord.JavacordHandler;
+import com.github.lkapitman.util.sdcf4j.CommandHandler;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.ChannelCategory;
 
 /**
  * The type App.
  */
 public class App {
 
+    private static DiscordApi api;
+
     /**
      * Main.
      *
      * @param args the args
      */
-    public static void main(final String[] args) {
-        DiscordApi api = new DiscordApiBuilder().setToken(args[0]).login().join();
 
+    public static void main(final String[] args) {
+        api = new DiscordApiBuilder().setToken(args[0]).login().join();
+        CommandHandler cmdHandler = new JavacordHandler(api);
+
+        cmdHandler.registerCommand(new SayCommand());
         api.addListener(new PingCommand());
         api.addListener(new CreateChannelCommand());
+    }
+
+    public static ChannelCategory channelCategory(String name) {
+        for (ChannelCategory cc : api.getChannelCategories()) {
+            if (cc.getName().equalsIgnoreCase(name))
+                return cc;
+        }
+        return null;
     }
 
 }
--- app/src/main/java/com/github/lkapitman/command/CreateChannelCommand.java
+++ app/src/main/java/com/github/lkapitman/command/CreateChannelCommand.java
@@ -1,49 +1,79 @@
 package com.github.lkapitman.command;
 
+import com.github.lkapitman.App;
 import com.vdurmont.emoji.EmojiParser;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.channel.ServerTextChannelBuilder;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.channel.ServerVoiceChannelBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.awt.*;
+
 public class CreateChannelCommand implements MessageCreateListener {
+
+    private static String type;
+    private static String category;
+    private static String name;
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        final String[] args = event.getMessage().getContent().split("\\s");
+        if (event.getMessage().getContent().equalsIgnoreCase(";create")) {
+            final String[] args = event.getMessage().getContent().split("\\s");
 
-        if (event.getMessage().getContent().startsWith(";create")) {
-            if (!(args.length > 2)) {
-                event.getChannel().sendMessage("Правильное использование: \n ;create <name> <text/voice> ");
-                return;
-            }
-        }
+            if (args[0].equalsIgnoreCase(";create")) {
+                if (!(args.length > 2)) {
+                    EmbedBuilder embed = new EmbedBuilder()
+                            .setTitle("Ошибка!")
+                            .setDescription("Неправильное использование!")
+                            .setAuthor("Manager")
+                            .addField("Использование", ";create <name> <text/voice> <category>")
+                            .setColor(Color.RED)
+                            .setFooter("Vladimir Putin Company. 2021");
 
-        String type = args[2];
+                    event.getChannel().sendMessage(embed);
+                    return;
+                } else {
+                    type = args[2];
+                    category = args[3];
+                    name = args[1];
+                }
+            }
 
-        switch (type) {
-            case "voice" -> {
-                ServerVoiceChannel voice = new ServerVoiceChannelBuilder(event.getServer().get()).setName(args[1]).create().join();
 
-                event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
+            if (App.channelCategory(category) == null) {
+                category = "БЕСЕДКА";
             }
-            case "text" -> {
-                ServerTextChannel text = new ServerTextChannelBuilder(event.getServer().get()).setName(args[1]).create().join();
 
-                event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
+            switch (type) {
+                case "voice" -> {
+                    ServerVoiceChannel voice = new ServerVoiceChannelBuilder(event.getServer().get()).setName(name).setCategory(App.channelCategory(category)).create().join();
+
+                    event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
+                }
+                case "text" -> {
+                    ServerTextChannel text = new ServerTextChannelBuilder(event.getServer().get()).setName(name).setCategory(App.channelCategory(category)).create().join();
+
+                    event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsup:"));
+                }
+                case "" -> {
+                    EmbedBuilder embed = new EmbedBuilder()
+                            .setTitle("Ошибка!")
+                            .setDescription("Неправильный тип или категория!")
+                            .setAuthor("Manager")
+                            .addField("Типы", "text - текстовый \n voice - голосовой")
+                            .addField("Категории", "Такой категории не существует!")
+                            .setColor(Color.RED)
+                            .setFooter("Vladimir Putin Company. 2021");
+
+                    event.getChannel().sendMessage(embed);
+                    event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsdown:"));
+                }
             }
-            case "" -> {
-                return;
-            }
-            default -> {
-                event.getChannel().sendMessage(
-                        "Неправильный тип! \n" +
-                                "text - текстовый \n" +
-                                "voice - голосовой");
-                event.getMessage().addReaction(EmojiParser.parseToUnicode(":thumbsdown:"));
-            }
+        } else {
+            return;
         }
     }
 }
